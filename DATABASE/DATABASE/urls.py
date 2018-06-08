a4ca231f5e50046f8f7f57dbf143739dfd7999c6
ejcaguiat <joshuacaguiat@yahoo.com>
2018-06-08 14:13:14 +0800

    fixed uhhhh xD

--- DATABASE/DATABASE/urls.py
+++ DATABASE/DATABASE/urls.py
@@ -15,6 +15,11 @@ urlpatterns = [
     path('homepage/leaseedit/', views.leaseedit, name = 'leaseeditpage'),
     path('homepage/salesedit/', views.salesedit, name = 'saleseditpage'),
     path('homepage/logout/', views.logout, name = 'logout'),
-    path('homepage/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR')
+    path('homepage/salesform/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
+    path('homepage/salesview/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
+    path('homepage/leaseview/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
+    path('homepage/leaseregister/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
+    path('homepage/leaseedit/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
+    path('homepage/salesedit/UNAUTHORIZED_ACCESS/', views.ERROR, name = 'ERROR'),
     
 ]
